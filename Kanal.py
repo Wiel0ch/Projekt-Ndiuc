92d13ce681231daf367ac01bf7342c5826ce9200
Wiel0ch <kubawieloch11@gmail.com>
2025-11-18 09:28:38 +0100

    dodanie GE

--- Kanal.py
+++ Kanal.py
@@ -30,6 +30,58 @@ class BSCKanal(Kanal):
                 if bit == 1:
                     nowy_bajt |= (1 << i)
 
+        dane_wyjsciowe.append(nowy_bajt)
+        return bytes(dane_wyjsciowe)
+
+
+class GilbertElliottKanal(Kanal):
+    def __init__(self, p_G: float, p_B: float, p_GB: float, p_BG: float):
+        if not all(0.0 <= p <= 1.0 for p in [p_G, p_B, p_GB, p_BG]):
+            raise ValueError("Wszystkie prawdopodobieństwa muszą być w przedziale [0, 1]")
+
+        self.p_G = p_G
+        self.p_B = p_B
+        self.p_GB = p_GB  # P(G -> B)
+        self.p_BG = p_BG  # P(B -> G)
+
+        # Prawdopodobieństwa pozostania w stanach
+        self.p_GG = 1.0 - p_GB
+        self.p_BB = 1.0 - p_BG
+
+        # Kanał startuje w stanie DOBRYM
+        self.is_bad_state = False
+
+        print(f"INFO: Utworzono kanał G-E:")
+        print(f"      Stan G: p(błąd) = {p_G}, p(G->B) = {p_GB}")
+        print(f"      Stan B: p(błąd) = {p_B}, p(B->G) = {p_BG}")
+
+    def przeslij(self, dane_wejsciowe: bytes) -> bytes:
+        dane_wyjsciowe = bytearray()
+
+        for bajt in dane_wejsciowe:
+            nowy_bajt = 0
+            for i in range(8):
+                bit = (bajt >> i) & 1
+                #Określenie p(błędu) na podstawie obcengo stanu
+                aktualne_p_error = self.p_B if self.is_bad_state else self.p_G
+
+                #Symuloawnie błędu bitu
+                if random.random() < aktualne_p_error:
+                    bit = 1 - bit
+
+                if bit == 1:
+                    nowy_bajt |= (1 << i)
+
+                #przejście do następnego stanu
+                if self.is_bad_state:
+                    #stan  zły(B)
+                    if random.random() < self.p_BG:
+                        self.is_bad_state = False  # Przejście B -> G
+                else:
+                    #stan dobry(G)
+                    if random.random() < self.p_GB:
+                        self.is_bad_state = True  # Przejście G -> B
+
             dane_wyjsciowe.append(nowy_bajt)
 
         return bytes(dane_wyjsciowe)
--- test.py
+++ test.py
@@ -1,12 +1,34 @@
 from Nadajnik import Nadajnik
 from Odbiornik import Odbiornik
-from Kanal import BSCKanal
+from Kanal import BSCKanal, GilbertElliottKanal
 
 nadajnik = Nadajnik()
 odbiornik = Odbiornik()
 
-kanal = BSCKanal(p_error=0.002)
-
-dane = b"skibidibi sigma 67 b"
-wyslane = nadajnik.wyslij(kanal, dane)
+#Użycie kanału BSC
+print("--- TEST KANAŁU BSC ---")
+kanal_bsc = BSCKanal(p_error=0.002)
+dane = b"Test kanalu BSC, czy wszystko dziala poprawnie?"
+wyslane = nadajnik.wyslij(kanal_bsc, dane)
 odebrane = odbiornik.odbierz(wyslane)
+print("-" * 20 + "\n")
+
+
+#Użycie kanału Gilberta-Elliotta
+print("TEST KANAŁU GILBERTA-ELLIOTTA")
+
+kanal_ge = GilbertElliottKanal(
+    p_G=0.0001,   # Prawie idealnie w stanie G
+    p_B=0.1,      # 10% błędów w stanie B
+    p_GB=0.005,   # Średnio po 1/0.005 = 200 bitach przejście do B
+    p_BG=0.1      # Średnio po 1/0.1 = 10 bitach powrót do G
+)
+daneGE = b"aa."
+
+print("\n--- Transmisja 1 ---")
+wyslane1 = nadajnik.wyslij(kanal_ge, daneGE)
+odebrane1 = odbiornik.odbierz(wyslane1)
+
+print("\n--- Transmisja 2 ---")
+wyslane2 = nadajnik.wyslij(kanal_ge, daneGE)
+odebrane2 = odbiornik.odbierz(wyslane2)
